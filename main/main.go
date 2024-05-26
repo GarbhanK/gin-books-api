5fb70fe24c7a4639726a42fa17ca8bb9dded7d27
GarbhanK <garbhan@me.com>
2024-05-26 21:12:30 +0100

    added caching for firestore query endpoints

--- main/main.go
+++ main/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"os"
 	"io"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/garbhank/gin-books-api/controllers"
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 )
+
+
 
 func init() {
 	log.SetLevel(log.InfoLevel)
@@ -31,17 +36,22 @@ func init() {
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	// cache endpoints which calls the Firestore db
+	store := persistence.NewInMemoryStore(time.Second)
+	ttl := time.Minute * 1 // todo: os.GetEnv
+
 	v1 := r.Group("/v1")
 	{
 		v1.GET("/", controllers.Root)
 		v1.GET("/ping", controllers.Ping)
-		v1.GET("/books", controllers.FindBooks)
+		v1.GET("/books", cache.CachePage(store, ttl, controllers.FindBooks))
+		v1.GET("/books/author/", cache.CachePage(store, ttl, controllers.FindAuthor))
+		v1.GET("/books/title/", cache.CachePage(store, ttl, controllers.FindBook))
 		v1.POST("/books", controllers.CreateBook)
-		v1.GET("/books/author/", controllers.FindAuthor)
-		v1.GET("/books/title/", controllers.FindBook)
 		v1.DELETE("/books/", controllers.DeleteBook)
 		// v1.PATCH("books/:id", controllers.UpdateBook)
 	}
+
 
 	return r
 }
